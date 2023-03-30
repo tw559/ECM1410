69eeb962bb4a01773bb0f392afd5a78a916297ab
tw559 <126590494+tw559@users.noreply.github.com>
2023-03-30 15:41:55 +0100

    Update Comment.java

--- Comment.java
+++ Comment.java
@@ -11,15 +11,32 @@ public class Comment {
     String handle;
     int parentPostID;
     public String commentMessage;
-    int commentPopularity;
+    int commentEndorsement;
+    int commentComment;
 
     public Comment(int commentId, String handle, int parentPostID, String commentMessage) {
         this.commentId = commentId;
         this.handle = handle;
         this.parentPostID = parentPostID;
         this.commentMessage = commentMessage;
-        this.commentPopularity = commentPopularity;
+        this.commentEndorsement = commentEndorsement;
+        this.commentComment = commentComment;
     }
+
+    public int getCommentId() {return commentId;}
+    public String getHandle() {return handle;}
+    public int getParentPostId() {return parentPostID;}
+    public String getCommentMessage() {return commentMessage;}
+    public int getCommentEndorsement() {return commentEndorsement;}
+    public int getCommentComment() {return commentComment;}
+
+
+    public String toString() {
+        return (" ID: " + getCommentId() +
+                ".\n Account: " + getHandle() +
+                ".\n No. endorsements: " + getCommentEndorsement() +
+                " | No. comments: " + getCommentComment() +
+                ".\n Post Message: " + getCommentMessage());}
 
     public Comment createComment(int endorsementId, String handle, int parentPostID, String endorsedMessage) {
         Comment outputComment = new Comment(endorsementId, handle, parentPostID, endorsedMessage);
